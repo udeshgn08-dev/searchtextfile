653da666fef27f7ee3b86a60ab7188260b3065ab
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-10 11:33:55 +0530

    Add files via upload

---
+++ src/Echo.java
@@ -0,0 +1,12 @@
+package com.example.filesearch;
+
+public class Echo {
+
+    public static void main(String[] args) {
+	for(String var:args) {
+	    System.out.println(var);
+	}
+
+    }
+
+}
---
+++ src/MainMethod.java
@@ -0,0 +1,14 @@
+package com.example.filesearch;
+
+public class MainMethod {
+    /*
+     * Java runtime looks for a main method in your class and it runs the code
+     * inside that method
+     */
+    public static void main(String[] args) {
+	for (int i = 1; i <= 10; i++) {
+	    System.out.println("Number " + i);
+	}
+    }
+
+}
