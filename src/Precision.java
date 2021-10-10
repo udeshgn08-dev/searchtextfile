c99516614249a23f0904012a4a4a40340fd8a322
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-10 12:03:15 +0530

    Add files via upload

---
+++ src/Precision.java
@@ -0,0 +1,19 @@
+package com.example.filesearch;
+
+public class Precision {
+
+    public static void main(String[] args) {
+	precisionMeth();
+
+    }
+
+    private static void precisionMeth() {
+	
+	double valOne = 45.676785D;
+	System.out.println("Double " + valOne);
+	float valTwo = 45989677F;
+	System.out.println("Float " + valTwo);
+
+    }
+
+}
